08d83452cde6e938a433408c342daf90e9391c71
Dmitry Kalinin <mitya.kalinin1@gmail.com>
2022-12-27 22:08:40 +0700

    init

---
+++ gum_lab5/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace gum_lab5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var n = new Note();
+            var notes = new List<Note>
+            {
+                n,
+                new(),
+                new(),
+            };
+            NotebookGrid.ItemsSource = notes;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var n = new Note { Name = "Sas" };
+            var t = NotebookGrid.ItemsSource;
+            Console.WriteLine(n.Name);
+        }
+
+        private void SearchButton_OnClick(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
---
+++ gum_lab5/Note.cs
@@ -0,0 +1,15 @@
+﻿namespace gum_lab5;
+
+public class Note
+{
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+    public int Year { get; set; }
+
+    public Note()
+    {
+        Name = "Иван";
+        PhoneNumber = "88005553535";
+        Year = 1995;
+    }
+}
